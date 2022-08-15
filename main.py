5629e5ffde3dd37548399cd0ede5344e83428a9a
iveshvara <54269229+iveshvara@users.noreply.github.com>
2022-08-15 16:13:20 +0300

    ok

--- main.py
+++ main.py
@@ -62,47 +62,64 @@ async def parsing_url(url):
                 except Exception:
                     pass
 
-    return ''
+    return datetime.datetime.strptime('01.01.0001', '%d.%m.%Y'), ''
 
 
 async def scheduler():
-    aioschedule.every().day.do(get_info_schedule)
+    aioschedule.every().day.do(get_send_info)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
 
 
-async def get_info_schedule():
-    cursor.execute(f'SELECT id_user FROM users WHERE notifications = 1')
-    users_typle = cursor.fetchall()
+async def get_info(id_user=None):
+    if id_user is None:
+        cursor.execute(f'SELECT id_user FROM users WHERE notify = 1')
+        users_typle = cursor.fetchall()
+    else:
+        users_typle = ((id_user,),)
 
+    answer = []
     urls = ["https://sevenergo.net/news/plan.html", "https://sevenergo.net/news/official.html", "https://sevenergo.net/news/incident.html"]
     for url in urls:
         date, text = await parsing_url(url)
-        cursor.execute(f'SELECT * FROM notifications WHERE date = {date} AND text = {text}')
-        result = cursor.fetchone()
-        if result is not None:
-            return
+        if not text == '':
+            cursor.execute(f'SELECT * FROM notifications WHERE date = "{date}" AND text = "{text}"')
+            result = cursor.fetchone()
+            if result is None:
+                for i in users_typle:
+                    cursor.execute(f'INSERT INTO notifications (id_user, date, text, sent) VALUES ({i[0]}, "{date}", "{text}", 0)')
+                    connect.commit()
 
-        for i in users_typle:
-            cursor.execute(f'INSERT INTO notifications (id_user, date, text, sent) VALUES ({i[0]}, "{date}", "{text}", 0)')
-            connect.commit()
+        answer.append((date, text))
+
+    return answer
+
+
+async def get_send_info(users=None):
+    await get_info(users)
 
     cursor.execute(f'SELECT * FROM notifications WHERE sent = 0')
     notifications_typle = cursor.fetchall()
     for i in notifications_typle:
         await bot.send_message(chat_id=i[0], text=i[2], parse_mode='MarkdownV2', disable_web_page_preview=True)
+        cursor.execute(f'UPDATE notifications SET sent = 1')
+        connect.commit()
+
+    today = datetime.datetime.today()
+    tomorrow = today + datetime.timedelta(days=1)
+    tomorrow_text = tomorrow.strftime('%Y-%m-%d 00:00:00')
+    cursor.execute(f'SELECT * FROM notifications WHERE date = "{tomorrow_text}"')
+    notifications_typle = cursor.fetchall()
+    for i in notifications_typle:
+        await bot.send_message(chat_id=i[0], text=i[2], parse_mode='MarkdownV2', disable_web_page_preview=True)
 
 
-async def get_info():
-    url = "https://sevenergo.net/news/plan.html"
-    date1, text1 = await parsing_url(url)
-
-    url = "https://sevenergo.net/news/official.html"
-    date2, text2 = await parsing_url(url)
-
-    url = "https://sevenergo.net/news/incident.html"
-    date3, text3 = await parsing_url(url)
+async def get_info_user(id_user):
+    answer = await get_info(id_user)
+    date1, text1 = answer[0]
+    date2, text2 = answer[1]
+    date3, text3 = answer[2]
 
     text = text1
 
@@ -134,11 +151,11 @@ async def command_start(message: Message):
 
     if result is None:
         cursor.execute(
-            'INSERT INTO users (id_user, first_name, last_name, username, notifications) '
+            'INSERT INTO users (id_user, first_name, last_name, username, notify) '
             f'VALUES ({id_user}, "{first_name}", "{last_name}", "{username}", 1)')
     else:
         cursor.execute(f'UPDATE users SET first_name = "{first_name}", last_name = "{last_name}", '
-                       f'username = "{username}" WHERE id_user = {id_user}')
+                       f'username = "{username}", notify = 1 WHERE id_user = {id_user}')
     connect.commit()
 
     text = ''
@@ -146,11 +163,11 @@ async def command_start(message: Message):
         text = 'Здравствуйте\! Теперь вы подписаны на уведомления\.'
     elif message.text == '/stop':
         text = 'Вы больше не будете получать уведомления\.'
-        cursor.execute(f'UPDATE users SET notifications = 0')
+        cursor.execute(f'UPDATE users SET notify = 0')
         connect.commit()
     elif message.text == '/check':
         await message.answer('Ищем информацию, минуту\.\.\.', parse_mode='MarkdownV2', disable_web_page_preview=True)
-        text = await get_info()
+        text = await get_info_user(id_user)
         if text == '':
             text = 'Информации об отключениях электроэнергии не найдено.'
 
